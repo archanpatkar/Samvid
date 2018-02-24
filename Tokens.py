1f2e33ecb712733bfbded69a2fe52249b9606da7
Archan Patkar <archanpatkar@gmail.com>
2018-02-24 14:34:58 +0530

    Update

--- Tokens.py
+++ Tokens.py
@@ -4,4 +4,4 @@ Number = (int, float)     # A Scheme Number is implemented as a Python int or fl
 Atom   = (Symbol, Number) # A Scheme Atom is a Symbol or Number
 List   = list             # A Scheme List is implemented as a Python list
 Exp    = (Atom, List)     # A Scheme expression is an Atom or List
-Env    = dict             # A Scheme environment is a mapping of {variable: value}
+# Env    = dict             # A Scheme environment is a mapping of {variable: value}
