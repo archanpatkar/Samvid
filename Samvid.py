42dbe2b0a32e4fff176ece11ae5b3bb3ad1435c1
Archan Patkar <archanpatkar@gmail.com>
2018-02-24 14:34:53 +0530

    Update

--- Samvid.py
+++ Samvid.py
@@ -1,12 +1,23 @@
 from Tokens import *
+from Entities import Env
 from Enviroment import globalenv
+
+class Procedure:
+    def __init__(self , params , body , env):
+        self.params = params
+        self.body = body
+        self.env = env
+
+    def __call__(self, *args):
+        return eval( self.body , Env( self.params , args , self.env ) )
+
 
 def eval(exp,env = globalenv):
     if(isinstance(exp,Symbol)):
-        return env[exp]
-    elif(isinstance(exp,Number)):
+        return env.find(exp)[exp]
+    elif(not isinstance(exp, List)):
         return exp
-    if exp[0] == 'quote':   
+    elif exp[0] == 'quote':   
         _ , *args = exp       
         return " ".join(args)
     elif(exp[0] == "if"):
@@ -18,7 +29,21 @@ def eval(exp,env = globalenv):
     elif(exp[0] == "define"):
         ( _ , symbol , exp ) = exp
         env[symbol] = eval(exp,env)
+    elif(exp[0] == "set!"):
+        ( _ , symbol , exp ) = exp
+        env.find(symbol)[symbol] = eval(exp,env)
+    elif(exp[0] == "lambda"):
+        ( _ , params , *body ) = exp
+        return Procedure(params,body,env)
+    # elif(exp[0] == "block"):
+    #     outputs = []
+    #     for e in exp[1:]:
+    #         rev = eval(e,env)
+    #         if(rev != None):
+    #             outputs.append(rev)
+    #     return outputs
     else:
+        print("Function Name:",exp[0])
         proc = eval(exp[0],env)
         args = [ eval(arg,env) for arg in exp[1:] ]
         return proc(*args)
